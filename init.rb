f4c0648aa4f8448e0822e4be4735f9d956ca4d1c
KUDO Shunsuke <kudo.shunsuke.xx@gmail.com>
2018-08-09 13:16:33 +0000

    add menu item at project menu

--- init.rb
+++ init.rb
@@ -5,4 +5,18 @@ Redmine::Plugin.register :sample_plugin do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+
+  # --- from here to add the menu item on project menu ---
+  permission    :test001, # route symbol
+                {:test => [:index]},
+                :public => true
+
+  menu          :project_menu,
+                :test001, # route symbol
+                {:controller => 'test', :action => 'index'},
+                :caption => :label_menu_caption,
+                :after => :activity,
+                :param => :project_id
+  # --- to here to add the menu item on project menu ---
+
 end
